d319e8e1af739edca09c77ad17c6e890228903a7
benboguan <nanchuci023@gmail.com>
2023-08-30 15:29:56 +0800

    test

--- script/diy-part2.sh
+++ script/diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改openwrt登陆地址,把下面的192.168.123.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.110.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把R30B1修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/ImmortalWrt/R30B1/g' package/base-files/files/bin/config_generate
