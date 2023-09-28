688d7036a2e596b029ba2af44f2f65c03d2a1f03
benboguan <nanchuci023@gmail.com>
2023-09-28 21:20:37 +0800

    test

--- script/diy-part2.sh
+++ script/diy-part2.sh
@@ -42,7 +42,7 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 ###### 取消bootstrap为默认主题 删除原luci中的argon ######
-sed -i 's/luci-theme-argon/luci-theme-argon-mod/g' feeds/luci/themes/luci-theme-argon/Makefile
+sed -i 's/Argon Theme/Argon Theme mod/g' feeds/luci/themes/luci-theme-argon/Makefile
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 
 ###### 删除原luci中的luci-app-passwall 下面rm -rf命令不起作用，另辟路径改变原包名称 ######
