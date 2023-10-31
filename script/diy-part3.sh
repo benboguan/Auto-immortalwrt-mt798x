f9dedcc1d8bfe97c642e98126ccf3ded3a3a7f71
benboguan <nanchuci023@gmail.com>
2023-10-31 19:34:16 +0800

    test

--- script/diy-part3.sh
+++ script/diy-part3.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改openwrt登陆地址,把下面的192.168.99.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把R30B1修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/TXD/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/TXD/g' package/base-files/files/bin/config_generate
 
 # 修改开源驱动wifi名称
 #sed -i 's/OpenWrt/R30B1_AX3000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
