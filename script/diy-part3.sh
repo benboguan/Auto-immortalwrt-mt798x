e96bde163b5c7959c4f4e3e6786c0afbac1084ef
benboguan <nanchuci023@gmail.com>
2023-11-01 10:53:53 +0800

    test

--- script/diy-part3.sh
+++ script/diy-part3.sh
@@ -38,6 +38,9 @@ sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai
 #修正连接数
 #sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
+#修改默认登录密码
+sed -i 's/root::0:0:99999:7:::/root:$1$Z/1UnJkW$kLW1Rti51WxDH.jtDJKPI.:19661:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 ###### 取消bootstrap为默认主题 ######
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
