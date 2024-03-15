49918c0fcade769fa9713a346e6733f984065148
benboguan <nanchuci023@gmail.com>
2024-03-16 02:06:45 +0800

    update

--- script/diy-part5.sh
+++ script/diy-part5.sh
@@ -21,7 +21,7 @@ sed -i 's/ImmortalWrt/360/g' package/base-files/files/bin/config_generate
 # 修改闭源驱动2G wifi名称
 #sed -i 's/ImmortalWrt-2.4G/MSG1500_2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #sed -i 's/MT7981_AX3000_2.4G/R30B1_AX3000_2.4G/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
-sed -i 's/$hostname-2.4G/MSG1500_2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/netifd/wireless/mtk.sh
+sed -i 's/$hostname-2.4G/MSG1500_2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
 
 # 修改闭源驱动5G wifi名称
 #sed -i 's/ImmortalWrt-5G/MSG1500_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
