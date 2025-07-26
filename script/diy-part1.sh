9725ae421c3d1ef4a4c9591e3e81b309a88446ab
benboguan <nanchuci023@gmail.com>
2025-07-26 23:05:09 +0800

    update

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -30,7 +30,7 @@ git pull
 
 # Add a feed source
 echo "src-git feeds_app https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+echo "src-git small https://github.com/benboguan/small" >> feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/{luci-app-alist,luci-app-adguardhome,luci-app-ipsec-vpnd,luci-app-ipsec-vpnserver-manyusers,luci-app-smartdns,luci-app-mosdns,luci-app-passwall,luci-app-ssr-plus,luci-app-vssr} && rm -rf feeds/packages/net/{alist,adguardhome,brook,hysteria,mosdns,smartdns,chinadns-ng,v2ray-core,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin,shadowsocks-rust,trojan-go,trojan-plus,trojan}
 rm -rf feeds/small/{luci-app-fchomo,luci-app-bypass,luci-app-nikki,luci-app-passwall2,mihomo,nikki}
 rm -rf feeds/packages/lang/golang
