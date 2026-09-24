e0d0cecca84ce5e0b80220ffd8fc9a6a5959c97a
benboguan <nanchuci023@gmail.com>
2026-09-24 20:43:18 +0800

    update

--- script/diy-part6.sh
+++ script/diy-part6.sh
@@ -10,10 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改openwrt登陆地址,把下面的192.168.111.1修改成你想要的就可以了
-sed -i 's/192.168.6.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.111.1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把Qihoo修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/Qihoo/g' package/base-files/files/bin/config_generate
+# 修改主机名字，把H5000M修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/ImmortalWrt/H5000M/g' package/base-files/files/bin/config_generate
 
 # 修改开源驱动wifi名称
 #sed -i 's/OpenWrt/R30B1_AX3000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -21,55 +21,49 @@ sed -i 's/ImmortalWrt/Qihoo/g' package/base-files/files/bin/config_generate
 # 修改闭源驱动2G wifi名称
 #sed -i 's/ImmortalWrt-2.4G/MSG1500_2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #sed -i 's/MT7981_AX3000_2.4G/R30B1_AX3000_2.4G/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
-sed -i 's/$hostname-2.4G/MSG1500_2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mtk.sh
-sed -i 's/OpenWRT-2.4G/MSG1500_2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mtk.sh
+#sed -i 's/ImmortalWrt_2.4G/H5000M_2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt-2.4G/H5000M_2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # 修改闭源驱动5G wifi名称
 #sed -i 's/ImmortalWrt-5G/MSG1500_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 #sed -i 's/MT7981_AX3000_5G/R30B1_AX3000_5G/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
-sed -i 's/$hostname-5G/MSG1500_5G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mtk.sh
-sed -i 's/OpenWRT-5G/MSG1500_5G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mtk.sh
+#sed -i 's/ImmortalWrt_5G/H5000M_5G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt-5G/H5000M_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
-# 添加个性信息
-#sed -i 's/R22.8.2/R22.8.2 by nanchuci/g' package/lean/default-settings/files/zzz-default-settings
-
-# 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-#rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
+# 修改闭源驱动6G wifi名称
+#sed -i 's/ImmortalWrt-5G/MSG1500_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+#sed -i 's/MT7981_AX3000_5G/R30B1_AX3000_5G/g' package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
+#sed -i 's/ImmortalWrt_6G/H5000M_6G/g' package/mtk/drivers/wifi-profile/files/common/mt79xx/lib/wifi/mac80211.uc
+#sed -i 's/ImmortalWrt-6G/H5000M_6G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # 更改时区
-sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-
-#修正连接数
-#sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
-
-###### 取消bootstrap为默认主题 ######
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 ###### 取消myddns_ipv4 ######
-sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
+#sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 
 ###### 加载补丁文件 ######
-for packagepatch in $( ls feeds/packages/feeds-package-patch ); do
-    cd feeds/packages/
-    echo Applying feeds-package-patch $packagepatch
-    patch -p1 --no-backup-if-mismatch < feeds-package-patch/$packagepatch
-    cd ../..
-done
-
-for smallpatch in $( ls feeds/small/feeds-luci-patch ); do
-    cd feeds/small/
-    echo Applying feeds-luci-patch $smallpatch
-    patch -p1 --no-backup-if-mismatch < feeds-luci-patch/$smallpatch
-    cd ../..
-done
-
-for lucipatch in $( ls feeds/luci/luci-patch ); do
-    cd feeds/luci/
-    echo Applying luci-patch $lucipatch
-    patch -p1 --no-backup-if-mismatch < luci-patch/$lucipatch
-    cd ../..
-done
-
+#for packagepatch in $( ls feeds/packages/feeds-package-patch ); do
+#    cd feeds/packages/
+#    echo Applying feeds-package-patch $packagepatch
+#    patch -p1 --no-backup-if-mismatch < feeds-package-patch/$packagepatch
+#    cd ../..
+#done
+#
+#for smallpatch in $( ls feeds/small/feeds-luci-patch ); do
+#    cd feeds/small/
+#    echo Applying feeds-luci-patch $smallpatch
+#    patch -p1 --no-backup-if-mismatch < feeds-luci-patch/$smallpatch
+#    cd ../..
+#done
+#
+#for lucipatch in $( ls feeds/luci/luci-patch ); do
+#    cd feeds/luci/
+#    echo Applying luci-patch $lucipatch
+#    patch -p1 --no-backup-if-mismatch < luci-patch/$lucipatch
+#    cd ../..
+#done
+#
 #for apppatch in $( ls feeds/feeds_app/feeds-app-patch ); do
 #    cd feeds/feeds_app/
 #    echo Applying feeds-app-patch $apppatch
@@ -78,61 +72,18 @@ done
 #done
 
 # Clone community packages to package/community
-mkdir package/community
-pushd package/community
-
-# Add Lienol's Packages
-#git clone --depth=1 https://github.com/Lienol/openwrt-package
-
-# Add luci-app-eqos
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-eqos
+mkdir -p package/mtk/applications
+pushd package/mtk/applications
 
 # Add dnsfilter
 #git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter
 
-# Add luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-#git clone --depth=1 https://github.com/kenzok8/small
-
-# Add luci-app-vssr <M>
-#git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-#git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr
-
-# Add mentohust & luci-app-mentohust
-#git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-#git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
-
 # Add lucky
 #git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
-
-# Add luci-app-natflow-users
-#svn co https://github.com/x-wrt/com.x-wrt/trunk/luci-app-natflow-users
-
-# Add natflow
-#svn co https://github.com/x-wrt/com.x-wrt/trunk/natflow
 
 # Add iptvhelper & luci-app-iptvhelper
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/iptvhelper
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper
-
-# Add luci-app-easymesh
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-easymesh
-
-# Add luci-app-pbr
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper
-
-# add luci-app-pptp-server
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pptp-server
-
-# add luci-app-pptpd
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pptpd
-
-# add quickstart
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/quickstart
-
-# add luci-app-quickstart
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-quickstart
 
 # add msd_lite
 #rm -rf feeds/packages/net/msd_lite
@@ -141,17 +92,11 @@ pushd package/community
 # add luci-app-msd_lite
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-msd_lite
 
-# Add luci-app-pushbot
-#svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-pushbot
-
 # Add luci-app-nat6-helper
 #git clone --depth=1 https://github.com/Ausaci/luci-app-nat6-helper
-
-# default luci-theme-argon
-#rm -rf feeds/luci/themes/luci-theme-bootstrap
-#rm -rf feeds/luci/themes/luci-theme-argon
-#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 
 # Add luci-theme-alpha
 #git clone --depth=1 https://github.com/derisamedia/luci-theme-alpha
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+
+popd
